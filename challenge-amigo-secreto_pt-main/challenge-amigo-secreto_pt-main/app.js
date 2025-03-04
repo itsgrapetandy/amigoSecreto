f5608a51609af5550b16a9ad1adcbe1601f08b60
Dante <dantegomes06@gmail.com>
2025-03-04 19:33:08 -0300

    projeto completo

--- challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let amigoEscolhido = document.querySelector("input").value;
+  if (amigoEscolhido == "") {
+    alert("Por favor, insira um valor válido.");
+  } else {
+    amigos.push(amigoEscolhido);
+    limparCampo();
+    mostrarAmigosNaLista();
+  }
+}
+
+function limparCampo() {
+  amigoEscolhido = document.querySelector("input");
+  amigoEscolhido.value = "";
+}
+
+function mostrarAmigosNaLista() {
+  paragrafoAmigos = document.getElementById("listaAmigos");
+  paragrafoAmigos.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li");
+    item.textContent = amigos[i];
+    paragrafoAmigos.appendChild(item);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length == 0) {
+    alert("Não há amigos suficientes para a realização do sorteio");
+  } else {
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `O amigo sorteado foi: ${sorteado}`;
+  }
+}
